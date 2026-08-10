747d790d3ec53722b84e7788cd73623c8c653327
Mohamed Atwan <atwan945@gmail.com>
2026-08-11 02:55:42 +0300

    fix: reset canvas filter properly on reset

--- main.js
+++ main.js
@@ -11,47 +11,49 @@ let download = document.getElementById("download");
 let img = document.getElementById("img");
 
 let reset = document.querySelector("span");
-let imgBox = document.querySelector('.img-box');
+let imgBox = document.querySelector(".img-box");
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
- function resetValue() {
-    img.style.filter = 'none';
-    saturate.value = 100;
-    contrast.value = 100;
-    brightness.value = 100;
-    sepia.value = 0;
-    grayscale.value = 0;
-    blur.value = 0;
-    hueRotate.value = 0;
- }
+function resetValue() {
+  saturate.value = 100;
+  contrast.value = 100;
+  brightness.value = 100;
+  sepia.value = 0;
+  grayscale.value = 0;
+  blur.value = 0;
+  hueRotate.value = 0;
 
-window.onload = function(){
-    download.style.display = 'none';
-    reset.style.display = 'none';
-    imgBox.style.display = 'none';
+  ctx.filter = "none";
+  ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 }
-upload.onchange = function(){
-    resetValue()
-       download.style.display = 'block';
-    reset.style.display = 'block';
-    imgBox.style.display = 'block';
-    let file = new FileReader();
-    file.readAsDataURL(upload.files[0]);
-        file.onload = function(){
+
+window.onload = function () {
+  download.style.display = "none";
+  reset.style.display = "none";
+  imgBox.style.display = "none";
+};
+upload.onchange = function () {
+  resetValue();
+  download.style.display = "block";
+  reset.style.display = "block";
+  imgBox.style.display = "block";
+  let file = new FileReader();
+  file.readAsDataURL(upload.files[0]);
+  file.onload = function () {
     img.src = file.result;
-    }
-    img.onload = function(){
-        canvas.width = img.width;
-        canvas.height = img.height;
-        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-        img.style.display = 'none';
-    }
-}
+  };
+  img.onload = function () {
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    img.style.display = "none";
+  };
+};
 let filters = document.querySelectorAll("ul li input");
-filters.forEach(filter => {
-filter.addEventListener("input", function(){
+filters.forEach((filter) => {
+  filter.addEventListener("input", function () {
     ctx.filter = `saturate(${saturate.value}%)
     contrast(${contrast.value}%)
     brightness(${brightness.value}%)
@@ -59,11 +61,10 @@ filter.addEventListener("input", function(){
     grayscale(${grayscale.value})
     blur(${blur.value}px)
     hue-rotate(${hueRotate.value}deg)
-    `
-     ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-})
-
-})
-download.onclick = function(){
-    download.href = canvas.toDataURL('image\jpeg');
-}
+    `;
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+  });
+});
+download.onclick = function () {
+  download.href = canvas.toDataURL("image\jpeg");
+};
